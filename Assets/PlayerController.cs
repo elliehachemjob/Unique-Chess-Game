0f4b54cab7e7f97e961bc740ada39be9355c287a
“Eliie” <elliehachemjob@gmail.com>
2024-06-11 11:56:24 +0200

    adding-playerController-conde

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,13 +15,28 @@ public class PlayerController : MonoBehaviour {
 		velocity = _velocity;
 	}
 
-	public void LookAt(Vector3 lookPoint) {
-		Vector3 heightCorrectedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
-		transform.LookAt (heightCorrectedPoint);
-	}
+    /* Vector3 velocity;
+    Rigidbody myRigidbody;
 
-	public void FixedUpdate() {
-		myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
 
-	}
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+
+    } */
 }
