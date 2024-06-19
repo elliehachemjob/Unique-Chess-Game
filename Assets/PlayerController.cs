3894dfefdeb9c29561df77e4a0541d1b5701cdbd
“Eliie” <elliehachemjob@gmail.com>
2024-06-19 13:10:46 +0200

    adding-player-controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Rigidbody))]
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
 
 	Vector3 velocity;
+	Rigidbody myRigidbody;
+
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void Move(Vector3 _velocity)
+	{
+		velocity = _velo}
+
+
+
+	/* Vector3 velocity;
 	Rigidbody myRigidbody;
 
 	void Start () {
@@ -23,5 +38,5 @@ public class PlayerController : MonoBehaviour {
 	public void FixedUpdate() {
 		myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
 
-	}
+	} */
 }
