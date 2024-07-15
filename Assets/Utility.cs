5666baa08f63e4a96719d4dc6a77c3bec967651b
“Eliie” <elliehachemjob@gmail.com>
2024-07-15 13:15:55 +0200

    utility-code

--- Assets/Utility.cs
+++ Assets/Utility.cs
@@ -2,23 +2,7 @@
 
 public static class Utility {
 
-
-
-    public static T[] ShuffleArray<T>(T[] array, int seed)
-    {
-        System.Random prng = new System.Random(seed);
-
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            int randomIndex = prng.Next(i, array.Length);
-            T tempItem = array[randomIndex];
-            array[randomIndex] = array[i];
-            array[i] = tempItem;
-        }
-
-        return array;
-    }
-    /* public static T[] ShuffleArray<T>(T[] array, int seed) {
+     public static T[] ShuffleArray<T>(T[] array, int seed) {
 		System.Random prng = new System.Random (seed);
 
 		for (int i =0; i < array.Length -1; i ++) {
@@ -28,7 +12,7 @@ public static class Utility {
 			array[i] = tempItem;
 		}
 
-		return array; */ 
+		return array; 
 	}
 
 }
