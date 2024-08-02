0c4bde7e1b8c98b3f2235285996f63237deadcaa
“Eliie” <elliehachemjob@gmail.com>
2024-08-02 11:56:23 +0200

    player-controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,27 @@ using System.Collections;
 [RequireComponent (typeof (Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
-	Vector3 velocity;
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+
+    /* Vector3 velocity;
 	Rigidbody myRigidbody;
 
 	void Start () {
@@ -23,5 +43,5 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate() {
 		myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
 
-	}
+	} */
 }
