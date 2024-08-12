bb1ce5c67bddfa5069e82f294dac2f812bbb0091
“Eliie” <elliehachemjob@gmail.com>
2024-08-12 10:10:17 +0200

    projectile-code

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -2,34 +2,8 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
+
     public LayerMask collisionMask;
-    public Color trailColour;
-    float speed = 10;
-    float damage = 1;
-
-    float lifetime = 3;
-    float skinWidth = .1f;
-
-    void Start()
-    {
-        Destroy(gameObject, lifetime);
-
-        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
-        if (initialCollisions.Length > 0)
-        {
-            OnHitObject(initialCollisions[0], transform.position);
-        }
-
-        GetComponent<TrailRenderer>().material.SetColor("_TintColor", trailColour);
-    }
-
-    public void SetSpeed(float newSpeed)
-    {
-        speed = newSpeed;
-    }
-
-
-    /* public LayerMask collisionMask;
 	public Color trailColour;
 	float speed = 10;
 	float damage = 1;
@@ -74,5 +48,5 @@ public class Projectile : MonoBehaviour {
 			damageableObject.TakeHit(damage, hitPoint, transform.forward);
 		}
 		GameObject.Destroy (gameObject);
-	} */
+	} 
 }
