6974fcdf5df5a1b6f2bbef16cb2898733da8689b
“Eliie” <elliehachemjob@gmail.com>
2024-08-18 14:22:10 +0200

    music-manager

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -22,7 +22,30 @@ public class MusicManager : MonoBehaviour {
 		}
 	}
 
-	void PlayMusic() {
+
+
+    /* public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    string sceneName;
+
+    void Start()
+    {
+        OnLevelWasLoaded(0);
+    }
+
+
+    void OnLevelWasLoaded(int sceneIndex)
+    {
+        string newSceneName = SceneManager.GetActiveScene().name;
+        if (newSceneName != sceneName)
+        {
+            sceneName = newSceneName;
+            Invoke("PlayMusic", .2f);
+        }
+    }
+
+    void PlayMusic() {
 		AudioClip clipToPlay = null;
 
 		if (sceneName == "Menu") {
@@ -36,6 +59,6 @@ public class MusicManager : MonoBehaviour {
 			Invoke ("PlayMusic", clipToPlay.length);
 		}
 
-	}
+	}*/
 
 }
