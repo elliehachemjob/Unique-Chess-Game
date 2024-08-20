337121d6663a36008540c929cd3a273873442cd3
“Eliie” <elliehachemjob@gmail.com>
2024-08-20 11:28:33 +0200

    player-code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -17,14 +17,35 @@ public class Player : LivingEntity {
 		base.Start ();
 	}
 
-	void Awake() {
+	/* void Awake() {
 		controller = GetComponent<PlayerController> ();
 		gunController = GetComponent<GunController> ();
 		viewCamera = Camera.main;
 		FindObjectOfType<Spawner> ().OnNewWave += OnNewWave;
 	}
 
-	void OnNewWave(int waveNumber) {
+    public float moveSpeed = 5;
+
+    public Crosshairs crosshairs;
+
+    Camera viewCamera;
+    PlayerController controller;
+    GunController gunController;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Awake()
+    {
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+        FindObjectOfType<Spawner>().OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber) {
 		health = startingHealth;
 		gunController.EquipGun (waveNumber - 1);
 	}
@@ -72,5 +93,5 @@ public class Player : LivingEntity {
 		AudioManager.instance.PlaySound ("Player Death", transform.position);
 		base.Die ();
 	}
-		
+		*/
 }
