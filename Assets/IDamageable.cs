02a947d1f2cd370e7a53893bf6a9557c6705c3b2
“Eliie” <elliehachemjob@gmail.com>
2024-08-21 12:12:51 +0200

    interface

--- Assets/IDamageable.cs
+++ Assets/IDamageable.cs
@@ -7,3 +7,12 @@ public interface IDamageable {
 	void TakeDamage (float damage);
 
 }
+
+/* public interface IDamageable
+{
+
+    void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection);
+
+    void TakeDamage(float damage);
+
+}/*
