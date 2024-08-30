bb6ea2e4d88645f6f328ef14cf50410898033e06
“Eliie” <elliehachemjob@gmail.com>
2024-08-30 17:06:32 +0200

    scorekeeper

--- Assets/ScoreKeeper.cs
+++ Assets/ScoreKeeper.cs
@@ -3,30 +3,7 @@ using System.Collections;
 
 public class ScoreKeeper : MonoBehaviour {
 
-	public static int score { get; private set; }
-	float lastEnemyKillTime;
-	int streakCount;
-	float streakExpiryTime = 1;
-
-	void Start() {
-		Enemy.OnDeathStatic += OnEnemyKilled;
-		FindObjectOfType<Player> ().OnDeath += OnPlayerDeath;
-	}
-
-	void OnEnemyKilled() {
-
-		if (Time.time < lastEnemyKillTime + streakExpiryTime) {
-			streakCount++;
-		} else {
-			streakCount = 0;
-		}
-
-		lastEnemyKillTime = Time.time;
-
-		score += 5 + (int)Mathf.Pow(2,streakCount);
-	}
-
-    /* public static int score { get; private set; }
+    public static int score { get; private set; }
     float lastEnemyKillTime;
     int streakCount;
     float streakExpiryTime = 1;
@@ -56,6 +33,6 @@ public class ScoreKeeper : MonoBehaviour {
 
     void OnPlayerDeath() {
 		Enemy.OnDeathStatic -= OnEnemyKilled;
-	}*/
+	}
 	
 }
