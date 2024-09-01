fda847492362f2f65a497a117f184b3b6adfee56
“Eliie” <elliehachemjob@gmail.com>
2024-09-01 12:55:30 +0200

    utility

--- Assets/Utility.cs
+++ Assets/Utility.cs
@@ -3,25 +3,6 @@
 public static class Utility {
 
 	public static T[] ShuffleArray<T>(T[] array, int seed) {
-		System.Random prng = new System.Random (seed);
-
-		for (int i =0; i < array.Length -1; i ++) {
-			int randomIndex = prng.Next(i,array.Length);
-			T tempItem = array[randomIndex];
-			array[randomIndex] = array[i];
-			array[i] = tempItem;
-		}
-
-		return array;
-	}
-
-}
-
-/* public static class Utility
-{
-
-    public static T[] ShuffleArray<T>(T[] array, int seed)
-    {
         System.Random prng = new System.Random(seed);
 
         for (int i = 0; i < array.Length - 1; i++)
@@ -33,5 +14,7 @@ public static class Utility {
         }
 
         return array;
-    }
-*/
+    } // done understood and went through
+
+}
+
